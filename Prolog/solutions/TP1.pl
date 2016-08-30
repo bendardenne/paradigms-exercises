47d5020d7fdcf791759ec4bf5db338fd0d69ca0f
Benoît Dardenne <benoit.dardenne@gmail.com>
2016-08-30 20:49:08 +0200

    Debut prolog

---
+++ Prolog/solutions/TP1.pl
@@ -0,0 +1,64 @@
+
+% Facts. 
+
+
+%%% memo perso
+% albert -> albert
+% prosper -> george
+% eufrasie -> cecilia
+% maria -> maria
+% jef ->  howard
+% francois -> roger
+% anita -> sarah
+% charel -> bob
+% josephine -> laura
+% tamara -> tamara 
+% alexandra -> alexandra
+% winnie -> jessica
+% bram -> brandon
+% inge -> nadia
+% frank -> frank
+% willem -> anthony 
+
+
+father(george,maria).
+father(george,howard).
+father(george,roger).
+father(george,laura).
+father(albert,tamara).
+father(albert,alexandra).
+father(albert,jessica).
+father(roger,brandon).
+father(roger,nadia).
+father(bob,frank).
+father(bob,anthony).
+
+mother(cecilia,maria).
+mother(cecilia,howard).
+mother(cecilia,roger).
+mother(cecilia,laura).
+mother(maria,tamara).
+mother(maria,alexandra).
+mother(maria,jessica).
+mother(sarah,brandon).
+mother(sarah,nadia).
+mother(laura,frank).
+mother(laura,anthony).
+
+male(george).
+male(albert).
+male(roger).
+male(howard).
+male(bob).
+male(brandon).
+male(frank).
+male(anthony).
+
+female(cecilia).
+female(maria).
+female(sarah).
+female(laura).
+female(tamara).
+female(alexandra).
+female(jessica).
+female(nadia).
---
+++ Prolog/solutions/tests.pl
@@ -0,0 +1,4 @@
+beautiful.
+prolog_is_simple.
+likes(student, prolog).
+cat(tom).
